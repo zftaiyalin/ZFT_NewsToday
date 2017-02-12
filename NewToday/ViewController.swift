a1867d88d2439cf01485174a9cc51ab9cc9e09d5
安风 <anfeng@anfengdeMacBook-Pro.local>
2017-02-12 10:28:06 +0800

    Initial Commit

---
+++ NewToday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewToday
+//
+//  Created by 安风 on 2017/2/12.
+//  Copyright © 2017年 安风. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
